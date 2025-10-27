5b5c1b354b47aadd736d54f730c036dd137ccba4
Darius Kaymak <darius.kaymak@gmail.com>
2025-10-27 06:19:03 +0100

    AAAAAAAAAAAAAH JAI COMPRIS

--- GNL/test.c
+++ GNL/test.c
@@ -1,32 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dakaymak <dakaymak@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/26 05:28:09 by dakaymak          #+#    #+#             */
-/*   Updated: 2025/10/26 05:35:23 by dakaymak         ###   ########.fr       */
+/*   Created: 2025/10/27 06:05:05 by dakaymak          #+#    #+#             */
+/*   Updated: 2025/10/27 06:05:05 by dakaymak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
-int	main()
+char	*ft_read(char *str, int fd)
+{
+	int end;
+
+	end = read(fd, str, 40);
+	str[end] = '\0';
+	return (str);
+}
+
+int	main(int ac, char **av)
 {
 	int		fd;
 	char	*str;
 	int		i;
 
 	i = 0;
-	fd = open("main.c", O_RDONLY);
-	while (i < 25)
+	fd = open("test.c", O_RDONLY);
+	str = malloc((sizeof (char)) * (40 + 1));
+	while (i < atoi(av[1]))
 	{
-		str = get_next_line(fd);
+		str = ft_read(str, fd);
 		printf("%s", str);
-		free(str);
 		i++;
 	}
+	free(str);
+	return (0);
 }
